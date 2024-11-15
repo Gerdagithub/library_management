4cedb95f87dbbe8d00f47964250d7535ddb46684
Gerdagithub <gerdaliukas98@gmail.com>
2024-11-15 10:39:27 +0900

    loan and user tables added to db

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Reader(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
