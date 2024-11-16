0fcd24ed6774263401c7c71e1b8a3bba74a17256
Gerdagithub <gerdaliukas98@gmail.com>
2024-11-16 16:17:59 +0900

    implemented search functionality

---
+++ books/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        # Fields that need to be fillable by the user
+        fields = ['isbn', 'title', 'author', 
+                  'publisher', 'publication_date', 
+                  'genre', 'copies_in_stock']
+        
+class SearchForm(forms.Form):
+    SEARCH_CHOICES = (
+        ('isbn', 'ISBN'),
+        ('title', 'Title'),
+        ('author', 'Author'),
+        ('publisher', 'Publisher'),
+    )
+    search_field = forms.ChoiceField(
+        choices=SEARCH_CHOICES,
+        error_messages={
+            'required': ''  # Suppress the error message for required field
+        },
+    )
+    search_query = forms.CharField(
+        max_length=255,
+        error_messages={
+            'required': ''  # Suppress the error message for required field
+        },
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your query'})  # Optional: Add placeholder
+    )
--- library_management/settings.py
+++ library_management/settings.py
@@ -75,7 +75,8 @@ ROOT_URLCONF = 'library_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        # 'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
